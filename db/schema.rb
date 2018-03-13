31c8cd75cd0e003d67bebee18dcb2d3dd14467f0
mariatunas <m.tunas@usp.ceu.es>
2018-03-13 09:32:10 +0000

    Task entity scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180313093054) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
